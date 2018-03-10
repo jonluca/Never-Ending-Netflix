748f52aec2f5e1024a0b5996678f1691c3c71d9c
JonLuca DeCaro <jdecaro@usc.edu>
2018-03-10 13:13:01 -0800

    search, credits

--- README.md
+++ README.md
@@ -5,13 +5,21 @@ NEN is a chrome extension that improves the Netflix experience for power users. 
 * Automatically skip intros
 * Automatically play the next episode
 * Skip the popups that ask if you're "Still here?" after 8 hours
+* Search *all* of Netflix's genre's
 
 ## Options
 
 NEN allows you to customize it's functionality with an options menu, which can be accessed by clicking the logo in the top bar.
 
-![options](https://i.imgur.com/AZimg0t.png)
+![options](https://i.imgur.com/0C0WoTy.png)
 
 ## Live link
 
 [Link to Chrome Webstore](https://chrome.google.com/webstore/detail/hdadmgabliibighlbejhlglfjgplfmhb)
+
+## Libraries & Credits
+
+* [Fuse.js](http://fusejs.io/) - Genre search
+* [gumby](https://gumbyframework.com/docs/javascript/) - Options page
+* [modernizr](https://modernizr.com/) - Options page
+* [jQuery](https://jquery.com/) 
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,11 @@
-var options;
+let options;
+let fuse;
+
 $(() => {
+  $('body').on('click', 'a', function () {
+    chrome.tabs.create({url: $(this).attr('href')});
+    return false;
+  });
   loadOptions(function (recOptions) {
     options = recOptions;
     $("#chkTitleSequence").prop('checked', options.skipTitleSequence);
@@ -10,8 +16,51 @@ $(() => {
     $('input').parent().on('gumby.onChange', function () {
       changeOption(this);
     });
+
+    $.ajax({
+      method: 'GET',
+      url: chrome.runtime.getURL("data/genres.json"),
+      type: 'json',
+      success: function (data, textStatus, jqXHR) {
+        //Handle data and status code here
+        let options = {
+          shouldSort: true,
+          threshold: 0.6,
+          location: 0,
+          distance: 100,
+          maxPatternLength: 32,
+          minMatchCharLength: 3,
+          keys: ["genre"]
+        };
+        fuse = new Fuse(data, options); // "list" is the item array
+      }
+    });
+
+    $('#genreSearch').on('keyup', function () {
+      $("#results").html("");
+      if (this.value.length > 1) {
+        /* Clear div results*/
+        // do search for this.value here
+        var results = fuse.search(this.value);
+        if (results.length) {
+          for (let i = 0; i < 100; i++) {
+            if (results.length === i - 1) {
+              break;
+            }
+            let entry = constructResultDiv(results[i]);
+            $("#results").append(entry);
+          }
+        }
+      }
+    });
   });
 });
+
+function constructResultDiv(elem) {
+  let divString = `<div class='entry'><a href="${elem.link}">${elem.genre}</a></div>`;
+  return divString;
+
+}
 
 function changeOption(elem) {
   switch (elem.htmlFor) {
@@ -26,6 +75,7 @@ function changeOption(elem) {
       break;
   }
   saveOptions();
+
 }
 
 function saveOptions() {
