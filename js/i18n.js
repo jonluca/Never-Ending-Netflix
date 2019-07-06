b2a9fbad781251dbf6c90fdb4dbe7056313c1b98
JonLuca DeCaro <jdecaro@usc.edu>
2019-07-06 15:18:59 -0700

    notes

--- js/i18n.js
+++ js/i18n.js
@@ -3,6 +3,8 @@
  *
  * Label any feed that needs to be internationalized with the class "i18n". Then the id of the element should
  * correspond to the _locales/lang/messages.json top level key, and will be replaced with that keys message.
+ *
+ * If the language lookup fails, it defaults to the english string embedded within the HTML
  */
 
 $(() => {
