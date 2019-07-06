d095461ac662799f0e295331a0c4ac955dde2533
JonLuca DeCaro <jdecaro@usc.edu>
2019-07-06 15:11:49 -0700

    Added translations

---
+++ js/i18n.js
@@ -0,0 +1,26 @@
+/**
+ * Internationalization Suite
+ *
+ * Label any feed that needs to be internationalized with the class "i18n". Then the id of the element should
+ * correspond to the _locales/lang/messages.json top level key, and will be replaced with that keys message.
+ */
+
+$(() => {
+  $('.i18n').each((i, e) => {
+    try {
+      $(e).text(chrome.i18n.getMessage($(e).attr('id')));
+    } catch (err) {
+      console.log(e);
+      console.log(err);
+    }
+  });
+
+  $(".i18n-placeholder").each((i, e) => {
+    try {
+      $(e).attr('placeholder', chrome.i18n.getMessage($(e).attr('id')));
+    } catch (err) {
+      console.log(e);
+      console.log(err);
+    }
+  });
+});
